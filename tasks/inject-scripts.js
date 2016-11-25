dcb8a181695cb541fb7a7b00361da1827ad11cb9
Diegoni <romain_diegoni@hotmail.com>
2016-11-25 15:30:18 +0000

    restore injection internal scripts in index.html

--- tasks/inject-scripts.js
+++ tasks/inject-scripts.js
@@ -5,9 +5,9 @@ module.exports = function(gulp, plugins, config) {
         gulp.task('inject-scripts', function() {
           var sources = gulp.src(config.deployment.scripts + '/internal.min.js', {read: false});
 
-          return gulp.src(config.deployment.views + '/index.html')
+          return gulp.src(config.deployment.root + '/index.html')
               .pipe(plugins.inject(sources))
-              .pipe(gulp.dest(config.deployment.views));
+              .pipe(gulp.dest(config.deployment.root));
         });
     };
 };
