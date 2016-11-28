60cfc921bd25ffd479cb376626eea515e7d323a9
Diegoni <romain_diegoni@hotmail.com>
2016-11-28 13:12:20 +0000

    minify css (missing part)

--- tasks/build-internal-styles.js
+++ tasks/build-internal-styles.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var argv = require('yargs').argv;
+
 module.exports = function(gulp, plugins, config) {
     return function() {
         return gulp.src(config.paths.styles)
           .pipe(plugins.sass())
+          .pipe(plugins.if(argv.production, plugins.csso()))
           .pipe(plugins.flatten())
           .pipe(gulp.dest(config.deployment.styles));
     };
