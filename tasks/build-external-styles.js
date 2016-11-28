e2c4ab5ac33a7a3dfe3bb915e6ddeed66e499370
Diegoni <romain_diegoni@hotmail.com>
2016-11-28 13:11:59 +0000

    minify css

--- tasks/build-external-styles.js
+++ tasks/build-external-styles.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var argv = require('yargs').argv;
+
 module.exports = function(gulp, plugins, config) {
     return function() {
         var scssFilter = plugins.filter('**/*.scss', { restore: true });
@@ -8,6 +10,7 @@ module.exports = function(gulp, plugins, config) {
             .pipe(plugins.mainBowerFiles({ overrides: config.bowerOverrides }))
             .pipe(scssFilter)
             .pipe(plugins.sass())
+            .pipe(plugins.if(argv.production, plugins.csso()))
             .pipe(plugins.flatten())
             .pipe(gulp.dest(config.deployment.styles));
     };
